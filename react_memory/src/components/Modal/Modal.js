ee82cf3d68d567f92e0e2b2f06cd6a5d55819a77
Ann Schulte <aschulte1919@gmail.com>
2018-10-17 19:32:12 -0600

    final push

---
+++ react_memory/src/components/Modal/Modal.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Modal.css';
+
+const btnStyle = {
+	background: "violet"
+  };
+
+function Modal() {
+	return (
+		<div className="instructionsModal">
+			<div className="modal-content">
+				<h4 className="center">Game Instructions</h4>
+				<ol>
+					<li>Click on an Image to start the game.</li>
+					<li>Every time you click an image, the images get rearranged.</li>
+					<li>If you pick the same image twice...You Lose!</li>
+					<li>Click all 12 images without repeating...You Win!</li>
+				</ol>
+			</div>
+			<br />
+			<div className="modal-footer white-text center">
+				<a className="waves-effect btn" style={btnStyle} onClick={() => {
+					document.getElementsByClassName('instructionsModal')[0].classList.add('hide');
+				}}>Let's Play Already...</a>
+			</div>	
+		</div>
+	)
+}
+
+
+
+
+
+export default Modal;
